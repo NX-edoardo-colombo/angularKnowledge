500fda77e600c1d858634d79e16a26e5de83124d
samuhuse <samuele.lombardi@nexusat.it>
2023-10-16 11:30:23 +0200

    fixed solution

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'knowledge'`, () => {
+  it(`should have as title 'angularKnowledge'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('knowledge');
+    expect(app.title).toEqual('angularKnowledge');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('knowledge app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularKnowledge app is running!');
   });
 });
