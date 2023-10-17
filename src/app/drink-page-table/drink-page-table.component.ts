064567c332460a61eb1c894ad1e49ec12d2e70d8
PC-COLOMBO\edoardo.colombo <edoardo.colombo@nexusat.it>
2023-10-17 10:29:38 +0200

    Added input and output between parent and child

--- src/app/drink-page-table/drink-page-table.component.ts
+++ src/app/drink-page-table/drink-page-table.component.ts
@@ -1,22 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DrinkTableService } from '../Services/drink-table.service';
 import { DrinksTableDto } from '../Services/DTOs/drink-table.dto';
+
 @Component({
   selector: 'kno-drink-page-table',
   templateUrl: './drink-page-table.component.html',
   styleUrls: ['./drink-page-table.component.scss']
 })
 export class DrinkPageTableComponent {
-  
-  arrayDrinks: DrinksTableDto[] = [];
+  @Input() arrayDrinks: DrinksTableDto[] = [];
 
-  constructor(private readonly drinkTableService : DrinkTableService) {
-    /* Without button */
-    /* this.drinkTableService.getCocktailList().subscribe(dto => this.arrayDrinks = dto.drinks) */
-  }
+  @Output() newItemEvent = new EventEmitter<MouseEvent>
 
-  onDrinkTableButtonClick(){
-    this.drinkTableService.getCocktailList().subscribe(dto => this.arrayDrinks = dto.drinks)
+  showList() {
+    this.newItemEvent.emit()
   }
-  
 }
--- src/app/drink-page/drink-page.component.ts
+++ src/app/drink-page/drink-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DrinkTableService } from '../Services/drink-table.service';
+import { DrinksTableDto } from '../Services/DTOs/drink-table.dto';
 
 @Component({
   selector: 'kno-drink-page',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class DrinkPageComponent {
 
+  drinkList: DrinksTableDto[] =[]
+
+  constructor(private readonly drinkTableService : DrinkTableService) {
+    /* Without button */
+    /* this.drinkTableService.getCocktailList().subscribe(dto => this.arrayDrinks = dto.drinks) */
+  }
+
+  onDrinkTableButtonClick(){
+    this.drinkTableService.getCocktailList().subscribe(dto => this.drinkList = dto.drinks)
+  }
 }
