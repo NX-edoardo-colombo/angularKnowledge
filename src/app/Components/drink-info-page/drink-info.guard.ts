60a2f44c5d47609741264d5905aa98c2d7098624
PC-COLOMBO\edoardo.colombo <edoardo.colombo@nexusat.it>
2023-11-03 09:04:20 +0100

    Added guard for forbidden-drinks

--- src/app/Components/drink-info-page/drink-info.guard.ts
+++ src/app/Components/drink-info-page/drink-info.guard.ts
@@ -1,7 +1,28 @@
-import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
 import { DrinkInfoPageService } from './drink-info-page.service';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class DrinkInfoGuard {
+  constructor(private drinkInfoPageService: DrinkInfoPageService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.drinkInfoPageService.isDrinkAlcholic$(route.params['id']).pipe(
+      map((isAlcoholic: boolean) => {
+        if (!isAlcoholic) {
+          this.router.navigate(['/forbidden-drink']);
+          return false;
+        }
+        return true;
+      })
+    );
+  }
+}
+/* 
 export const drinkInfoGuard: CanActivateFn = (route, state) => {
   return inject(DrinkInfoPageService).isDrinkAlcholic$(route.params['id'])
 };
+ */
